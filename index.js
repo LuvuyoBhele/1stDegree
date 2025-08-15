a77e9c5a8cb766c6407d093e6ba253c48f2545c6
LuvuyoBhele <113292877+LuvuyoBhele@users.noreply.github.com>
2025-08-15 10:47:27 +0200

    new files

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".drpBtn").click(function() {
+    $(".menu").slideToggle();
+    $(".menu").css("opacity", "0.5");
+});
+
+$(".menu").mouseover(function() {
+    $(this).css("opacity", "1");
+});
+
+$(".menu").mouseout(function() {
+    $(this).css("opacity", "0.5");
+});
