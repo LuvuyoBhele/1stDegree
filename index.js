e9f266dc990a65ba943215e8e4b846ad2a802ff2
LuvuyoBhele <113292877+LuvuyoBhele@users.noreply.github.com>
2025-08-23 18:44:38 +0200

    i fixed the previous mistakes that undone the first type. I also modified the design for bigger screens

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-$(".drpBtn").click(function() {
+$(".drpBtn").on("click",function() {
     $(".menu").slideToggle();
     $(".menu").css("opacity", "0.5");
 });
 
 $(".menu").mouseover(function() {
     $(this).css("opacity", "1");
+    $(this).css("height", "50px");
+    $(this).css("width", "12em");
 });
 
 $(".menu").mouseout(function() {
     $(this).css("opacity", "0.5");
+    $(this).css("height", "40px");
+    $(this).css("width", "10em");
+    
 });
 
-$(".drpBtn").click(function() {
-    $(".menu").slideToggle();
-    $(".menu").css("opacity", "0.5");
-});
+$(".menu").click(function(){
+    var _this = $(this)
 
-$(".menu").mouseover(function() {
-    $(this).css("opacity", "1");
-});
+    setTimeout(function() {
+    $(_this).css("opacity", "0.5");
+    $(_this).css("height", "40px");
+    $(_this).css("width", "10em");}, 5)
+})
 
-$(".menu").mouseout(function() {
-    $(this).css("opacity", "0.5");
-});
